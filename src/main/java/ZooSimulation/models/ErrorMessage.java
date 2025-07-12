c2465aefd86c615d4ab8b78ed5c721f6fbbe5a6a
jovi-isorena <49135614+jovi-isorena@users.noreply.github.com>
2025-07-13 07:23:10 +0800

    added EMPTY_SHOP message

--- src/main/java/ZooSimulation/models/ErrorMessage.java
+++ src/main/java/ZooSimulation/models/ErrorMessage.java
@@ -9,7 +9,8 @@ public enum ErrorMessage {
     HANDLER_NOT_FOUND("Handler not found."),
     VENDOR_NOT_FOUND("Vendor not found."),
     PERSON_ALREADY_EXISTS("Person is already assigned to a role."),
-    PRODUCT_ALREADY_EXISTS("Product is in the list.");
+    PRODUCT_ALREADY_EXISTS("Product is in the list."),
+    EMPTY_SHOP("No available products.");
 
     private final String description;
     ErrorMessage(String description){
