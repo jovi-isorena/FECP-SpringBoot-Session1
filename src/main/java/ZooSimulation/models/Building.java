f652009d6292758932884dee0f95e1851dfc30db
jovi-isorena <49135614+jovi-isorena@users.noreply.github.com>
2025-07-13 05:20:27 +0800

    added name attribute and constructor to building class.

--- src/main/java/ZooSimulation/models/Building.java
+++ src/main/java/ZooSimulation/models/Building.java
@@ -1,4 +1,21 @@
 package ZooSimulation.models;
 
 public class Building {
+    private String name;
+
+    public Building(){}
+    public Building(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
 }
