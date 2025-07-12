cadc6e14152a2fb67b343ab946aac2be51cf0732
jovi-isorena <49135614+jovi-isorena@users.noreply.github.com>
2025-07-12 16:33:41 +0800

    added blueprints for modules and implemented zoo methods

--- src/main/java/ZooSimulation/modules/AdminModule.java
+++ src/main/java/ZooSimulation/modules/AdminModule.java
@@ -1,25 +1,42 @@
 package ZooSimulation.modules;
 
+import ZooSimulation.models.Animal;
 import ZooSimulation.models.Manager;
+import ZooSimulation.models.Zoo;
 import ZooSimulation.views.AdminLoginView;
 import ZooSimulation.views.ManagerMainMenu;
 
 public class AdminModule {
-    Manager zooManager = new Manager("user1","password1");
+    private Zoo zoo;
 
-    public void start(){
+
+    //    Manager zooManager = new Manager("user1","password1");
+    public AdminModule(Zoo zoo){
+        this.zoo = zoo;
+    }
+
+    public Zoo start(){
         Manager manager = AdminLoginView.print();
         boolean valid = isManagerValid(manager);
         if(valid){
             ManagerMainMenu.print();
 
         }
+        return zoo;
+    }
 
+    public void addAnimal(){
+        Animal animal = AddAnimalView.print();
+        zoo.getAnimals().add(animal);
+    }
+
+    public Zoo getZoo() {
+        return zoo;
     }
 
     public boolean isManagerValid(Manager manager){
-        if(manager.getUserName() == zooManager.getUserName() &&
-        manager.getPassword() == zooManager.getPassword()){
+        if(manager.getUserName() == zoo.getManager().getUserName() &&
+        manager.getPassword() == zoo.getManager().getPassword()){
             return true;
         }
         else{
