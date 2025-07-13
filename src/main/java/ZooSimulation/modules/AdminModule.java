d9544608dd7cb54a259d20f7c4bbdcacd83d47c5
mittens417 <cadelazaro97@gmail.com>
2025-07-13 13:25:58 +0800

    Implemented changes done for access vendor module.

--- src/main/java/ZooSimulation/modules/AdminModule.java
+++ src/main/java/ZooSimulation/modules/AdminModule.java
@@ -1,34 +1,73 @@
 package ZooSimulation.modules;
 
-import ZooSimulation.models.Animal;
-import ZooSimulation.models.Manager;
-import ZooSimulation.models.Zoo;
-import ZooSimulation.views.AdminLoginView;
-import ZooSimulation.views.ManagerMainMenu;
+import ZooSimulation.models.*;
+import ZooSimulation.views.*;
+
+import java.util.Scanner;
 
 public class AdminModule {
     private Zoo zoo;
-
+    Scanner scanner = new Scanner(System.in);
 
     //    Manager zooManager = new Manager("user1","password1");
-    public AdminModule(Zoo zoo){
+    public AdminModule(Zoo zoo) {
         this.zoo = zoo;
     }
 
-    public Zoo start(){
+    public Zoo start() {
         Manager manager = AdminLoginView.print();
         boolean valid = isManagerValid(manager);
-        if(valid){
+        if (valid) {
             ManagerMainMenu.print();
-
+            return zoo;
         }
-        return zoo;
+
+//        Used for shop assigning, vendor validation, and vendor name input
+
+//        GiftShop giftShop = new GiftShop();
+//        FoodShop foodShop = new FoodShop();
+//        DrinksShop drinksShop = new DrinksShop();
+//
+        String vendorName = VendorLoginForm.print();
+        Vendor validVendor = isVendorValid(vendorName);
+//        Vendor assignVendor = Scanner.nextLine();
+//        if (validVendor != null){
+//            validVendor.getAssignedShop();
+//            assignVendor.setAssignedShop();
+//            return zoo;
+//            }
+
+        {
+            int choice;
+            VendorMenu.print();
+
+            do {
+                choice = scanner.nextInt();
+                switch (choice) {
+                    case 1:
+                        ListItemView.print(validVendor);
+                        break;
+
+                    case 2:
+                        AddItemView.print(validVendor);
+                        break;
+
+                    case 3:
+                        RemoveItemView.print(validVendor);
+                        break;
+
+                }
+            }while (choice !=4);
+            scanner.close();
+
+            return zoo;
+        }
     }
 
-    public void addAnimal(){
-        Animal animal = AddAnimalView.print();
-        zoo.getAnimals().add(animal);
-    }
+//    public void addAnimal(){
+//        Animal animal = AddAnimalView.print();
+//        zoo.getAnimals().add(animal);
+//    }
 
     public Zoo getZoo() {
         return zoo;
@@ -42,5 +81,14 @@ public class AdminModule {
         else{
             return false;
         }
+    }
+
+
+    public Vendor isVendorValid(String vendorName){
+       return (Vendor)zoo.getPeople()
+               .stream()
+               .filter(p -> p.getName().equals(vendorName))
+               .findFirst()
+               .orElse(null);
     }
 }
