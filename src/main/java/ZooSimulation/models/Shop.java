d86bd8692ff51cd5713e88b3251b889968d5a34a
mittens417 <cadelazaro97@gmail.com>
2025-07-13 13:24:53 +0800

    Implemented methods in Shop model, and created a work in progress filter for GiftShop model

--- src/main/java/ZooSimulation/models/Shop.java
+++ src/main/java/ZooSimulation/models/Shop.java
@@ -1,4 +1,41 @@
 package ZooSimulation.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Shop extends Building{
+    private List<String> products = new ArrayList<>();
+
+    public List<String> getShopProducts() {
+        return products;
+    }
+
+
+    private String shopType;
+
+    public String getShopType() {
+        return shopType;
+    }
+
+    public void setShopType(String shopType) {
+        this.shopType = shopType;
+    }
+
+    public void listItems(){
+        for(String product: products){
+            System.out.println(product);
+        }
+
+    }
+
+    public void addItems(String productName){
+        products.add(productName);
+
+    }
+
+    public void removeItems(String productName){
+        products.remove(productName);
+    }
+
 }
+
