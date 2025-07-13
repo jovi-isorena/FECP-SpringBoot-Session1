0d39c347155946f13ccede8ae7cffca69c821df7
mittens417 <cadelazaro97@gmail.com>
2025-07-13 13:23:58 +0800

    Assigned a constructor, getter, and setter for Vendor model

--- src/main/java/ZooSimulation/models/Vendor.java
+++ src/main/java/ZooSimulation/models/Vendor.java
@@ -1,8 +1,40 @@
 package ZooSimulation.models;
 
-public class Vendor {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-    public void sell(){
+public class Vendor extends Person {
 
+    public Vendor(String name) {
+        super(name);
     }
+
+    private Shop assignedShop;
+
+    public Shop getAssignedShop() {
+        return assignedShop;
+    }
+
+    public void setAssignedShop(Shop assignedShop) {
+        this.assignedShop = assignedShop;
+    }
+
 }
+
+//create a Product class
+//inside product class: product name, price (use hashmap)
+
+//Attribute for shop or ticket
+//use super() class
+
+
+
+
+
+
+
+
+
+
+
